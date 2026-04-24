91d01a5cfae12262254915270c488d3473b5c1c4
Abdullah Nadeem <abdullahsheikh6883@gmail.com>
2026-04-24 13:09:41 +0500

    Register TextToSpeechPlugin in MainActivity

--- android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
+++ android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
@@ -1,5 +1,23 @@
 package com.maahhha.formatforge;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import com.maahhha.formatforge.app.plugins.TextToSpeechPlugin;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        List<Class<? extends Plugin>> pluginClasses = new ArrayList<>();
+        pluginClasses.add(TextToSpeechPlugin.class);
+        // ... any other plugins you may have
+        
+        for (Class<? extends Plugin> pluginClass : pluginClasses) {
+            registerPlugin(pluginClass);
+        }
+    }
+}
