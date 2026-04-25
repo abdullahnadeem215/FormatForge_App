0fee7c91585de39eb2101096a8b1b981f35af916
Abdullah Nadeem <abdullahsheikh6883@gmail.com>
2026-04-25 16:15:01 +0500

    Import SubjectSegmentationPlugin in MainActivity

--- android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
+++ android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
@@ -2,7 +2,7 @@ package com.maahhha.formatforge;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-
+import com.getcapacitor.community.mlkit.subjectsegmentation.SubjectSegmentationPlugin;
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
