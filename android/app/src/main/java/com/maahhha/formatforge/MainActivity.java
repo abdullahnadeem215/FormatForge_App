16565c550516b5a974f56cca1890617abfad0317
Abdullah Nadeem <abdullahsheikh6883@gmail.com>
2026-04-25 16:19:27 +0500

    Simplify plugin registration in MainActivity
    Removed manual plugin registration for ML Kit background remover.

--- android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
+++ android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
@@ -2,23 +2,11 @@ package com.maahhha.formatforge;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import com.getcapacitor.community.mlkit.subjectsegmentation.SubjectSegmentationPlugin;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        List<Class<? extends Plugin>> pluginClasses = new ArrayList<>();
-        // Register ML Kit background remover plugin
-        pluginClasses.add(SubjectSegmentationPlugin.class);
-        // (other custom plugins can be added here later)
-
-        for (Class<? extends Plugin> pluginClass : pluginClasses) {
-            registerPlugin(pluginClass);
-        }
+        // No manual registration needed. Capacitor auto-discovers installed plugins.
     }
 }
