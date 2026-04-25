95de2c2fcc60a9151d0b79620c7ddc09f05e315e
Abdullah Nadeem <abdullahsheikh6883@gmail.com>
2026-04-25 21:56:37 +0500

    Reorder onCreate method in MainActivity
    Moved super.onCreate call after plugin registration.

--- android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
+++ android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
@@ -2,13 +2,11 @@ package com.maahhha.formatforge;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.maahhha.formatforge.plugins.BackgroundRemoverPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Register your custom plugin
         registerPlugin(BackgroundRemoverPlugin.class);
+        super.onCreate(savedInstanceState);
     }
 }
