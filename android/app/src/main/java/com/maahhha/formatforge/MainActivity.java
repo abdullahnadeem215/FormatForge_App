9adf16a88cc554f6ab0714ea82693528413658df
Abdullah Nadeem <abdullahsheikh6883@gmail.com>
2026-04-25 16:16:55 +0500

    Register ML Kit background remover plugin
    Added registration for ML Kit background remover plugin.

--- android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
+++ android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
@@ -2,12 +2,23 @@ package com.maahhha.formatforge;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
 import com.getcapacitor.community.mlkit.subjectsegmentation.SubjectSegmentationPlugin;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Capacitor automatically discovers community plugins
-        // No manual registration needed for @capacitor-community/text-to-speech
+
+        List<Class<? extends Plugin>> pluginClasses = new ArrayList<>();
+        // Register ML Kit background remover plugin
+        pluginClasses.add(SubjectSegmentationPlugin.class);
+        // (other custom plugins can be added here later)
+
+        for (Class<? extends Plugin> pluginClass : pluginClasses) {
+            registerPlugin(pluginClass);
+        }
     }
 }
