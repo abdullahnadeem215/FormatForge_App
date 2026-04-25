236411d4f7039be854471c28776e8b568992a3fb
Abdullah Nadeem <abdullahsheikh6883@gmail.com>
2026-04-25 16:57:39 +0500

    Simplify plugin registration in MainActivity
    Removed manual plugin registration for SelfieSegmentation.

--- android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
+++ android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
@@ -2,22 +2,11 @@ package com.maahhha.formatforge;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import com.capawesome.capacitorjs.plugins.mlkit.selfiesegmentation.SelfieSegmentation;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        List<Class<? extends Plugin>> pluginClasses = new ArrayList<>();
-        pluginClasses.add(SelfieSegmentation.class);
-        // ... other plugins if any
-
-        for (Class<? extends Plugin> pluginClass : pluginClasses) {
-            registerPlugin(pluginClass);
-        }
+        // Capacitor auto‑discovers installed plugins.
     }
 }
