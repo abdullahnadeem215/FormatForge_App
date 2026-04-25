8d31d132db6adb32e01d03e824485b6630833d48
Abdullah Nadeem <abdullahsheikh6883@gmail.com>
2026-04-25 19:12:13 +0500

    Add comment for custom plugin registration
    Added a comment to indicate where to register custom plugins.

--- android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
+++ android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Register your custom plugin
         registerPlugin(BackgroundRemoverPlugin.class);
     }
 }
