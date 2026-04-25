9035bccf2204f74d915d00eeeb0db8397df3a005
Abdullah Nadeem <abdullahsheikh6883@gmail.com>
2026-04-25 12:45:57 +0500

    Simplify plugin registration in MainActivity
    Removed manual registration of TextToSpeechPlugin and related code.

--- android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
+++ android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
@@ -1,26 +1,13 @@
 package com.maahhha.formatforge;
 
 import android.os.Bundle;
-import android.widget.Toast;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import com.maahhha.formatforge.app.plugins.TextToSpeechPlugin;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        List<Class<? extends Plugin>> pluginClasses = new ArrayList<>();
-        pluginClasses.add(TextToSpeechPlugin.class);
-
-        for (Class<? extends Plugin> pluginClass : pluginClasses) {
-            registerPlugin(pluginClass);
-        }
-
-        // Show a toast to confirm the plugin was registered (visible on app start)
-        Toast.makeText(this, "TextToSpeechPlugin registered", Toast.LENGTH_SHORT).show();
+        // Capacitor automatically discovers community plugins
+        // No manual registration needed for @capacitor-community/text-to-speech
     }
 }
