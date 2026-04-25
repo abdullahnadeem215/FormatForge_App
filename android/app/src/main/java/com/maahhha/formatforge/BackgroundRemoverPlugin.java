cbe5124f76ca5cfdd9cd485fd6294da7df374c64
Abdullah Nadeem <abdullahsheikh6883@gmail.com>
2026-04-26 00:03:06 +0500

    Refactor BackgroundRemoverPlugin to Java

---
+++ android/app/src/main/java/com/maahhha/formatforge/BackgroundRemoverPlugin.java
@@ -0,0 +1,16 @@
+package com.maahhha.formatforge.plugins;
+
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
+@CapacitorPlugin(name = "BackgroundRemover")
+public class BackgroundRemoverPlugin extends Plugin {
+
+    @PluginMethod
+    public void removeBackground(PluginCall call) {
+        // We'll fill this in the next step
+        call.unimplemented("Not implemented yet");
+    }
+}
