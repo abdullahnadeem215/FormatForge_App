abd4b6d156b19e37c3a70f2b82adf5c2b5bd0f8c
Abdullah Nadeem <abdullahsheikh6883@gmail.com>
2026-05-02 23:03:06 +0500

    Register DocxToPdfPlugin in MainActivity

--- android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
+++ android/app/src/main/java/com/maahhha/formatforge/MainActivity.java
@@ -6,8 +6,7 @@ import com.getcapacitor.BridgeActivity;
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(DocxToPdfPlugin.class);
         super.onCreate(savedInstanceState);
-        // Capacitor automatically discovers all installed plugins
-        // (including @capacitor-mlkit/subject-segmentation and @capacitor-community/text-to-speech)
     }
 }
